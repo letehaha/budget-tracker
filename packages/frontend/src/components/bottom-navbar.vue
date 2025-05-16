fd5e07bf71d35d6ff09acb2723f7427231079105
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2025-05-17 00:04:48 +0200

    feat: Reuse existing nav links component

--- packages/frontend/src/components/bottom-navbar.vue
+++ packages/frontend/src/components/bottom-navbar.vue
@@ -1,32 +1,11 @@
 <template>
   <div class="sticky bottom-0 left-0 right-0 py-2">
     <nav class="grid grid-cols-3 gap-1">
-      <router-link v-slot="{ isActive }" :to="{ name: ROUTES_NAMES.home }">
-        <ui-button :variant="isActive ? 'default' : 'ghost'" as="span" class="w-full gap-2 px-3" size="default">
-          <LayoutDashboardIcon />
-          <span class="max-sm:hidden"> Dashboard </span>
-        </ui-button>
-      </router-link>
-
-      <router-link v-slot="{ isActive }" :to="{ name: ROUTES_NAMES.accounts }">
-        <ui-button :variant="isActive ? 'default' : 'ghost'" as="span" class="w-full gap-2 px-3" size="default">
-          <LayersIcon />
-          <span class="max-sm:hidden"> Accounts </span>
-        </ui-button>
-      </router-link>
-
-      <router-link v-slot="{ isActive }" :to="{ name: ROUTES_NAMES.transactions }">
-        <ui-button :variant="isActive ? 'default' : 'ghost'" as="span" class="w-full gap-2 px-3" size="default">
-          <CreditCardIcon />
-          <span class="max-sm:hidden"> Transactions </span>
-        </ui-button>
-      </router-link>
+      <NavigationLinks bottom-nav />
     </nav>
   </div>
 </template>
 
 <script setup lang="ts">
-import UiButton from '@/components/lib/ui/button/Button.vue';
-import { ROUTES_NAMES } from '@/routes';
-import { CreditCardIcon, LayersIcon, LayoutDashboardIcon } from 'lucide-vue-next';
+import NavigationLinks from './sidebar/navigation-links.vue';
 </script>
