0880bc2fc11c7829f8693f517918514e035df0f2
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2025-05-25 14:14:24 +0200

    fix: navbar after merge

--- packages/frontend/src/components/sidebar/navigation-links.vue
+++ packages/frontend/src/components/sidebar/navigation-links.vue
@@ -7,53 +7,51 @@ withDefaults(defineProps<{ bottomNav?: boolean }>(), { bottomNav: false });
 </script>
 
 <template>
-  <nav>
-    <router-link v-slot="{ isActive }" :to="{ name: ROUTES_NAMES.home }">
-      <ui-button
-        :variant="isActive ? 'default' : 'ghost'"
-        as="span"
-        :class="['w-full gap-2 px-3', { 'justify-start': !bottomNav }]"
-        size="default"
-      >
-        <LayoutDashboardIcon />
-        <span :class="{ 'max-sm:hidden': bottomNav }"> Dashboard </span>
-      </ui-button>
-    </router-link>
+  <router-link v-slot="{ isActive }" :to="{ name: ROUTES_NAMES.home }">
+    <ui-button
+      :variant="isActive ? 'default' : 'ghost'"
+      as="span"
+      :class="['w-full gap-2 px-3', { 'justify-start': !bottomNav }]"
+      size="default"
+    >
+      <LayoutDashboardIcon />
+      <span :class="{ 'max-sm:hidden': bottomNav }"> Dashboard </span>
+    </ui-button>
+  </router-link>
 
-    <router-link v-slot="{ isActive }" :to="{ name: ROUTES_NAMES.accounts }">
-      <ui-button
-        :variant="isActive ? 'default' : 'ghost'"
-        as="span"
-        :class="['w-full gap-2 px-3', { 'justify-start': !bottomNav }]"
-        size="default"
-      >
-        <LayersIcon />
-        <span :class="{ 'max-sm:hidden': bottomNav }"> Accounts </span>
-      </ui-button>
-    </router-link>
+  <router-link v-slot="{ isActive }" :to="{ name: ROUTES_NAMES.accounts }">
+    <ui-button
+      :variant="isActive ? 'default' : 'ghost'"
+      as="span"
+      :class="['w-full gap-2 px-3', { 'justify-start': !bottomNav }]"
+      size="default"
+    >
+      <LayersIcon />
+      <span :class="{ 'max-sm:hidden': bottomNav }"> Accounts </span>
+    </ui-button>
+  </router-link>
 
-    <router-link v-slot="{ isActive }" :to="{ name: ROUTES_NAMES.transactions }">
-      <ui-button
-        :variant="isActive ? 'default' : 'ghost'"
-        as="span"
-        :class="['w-full gap-2 px-3', { 'justify-start': !bottomNav }]"
-        size="default"
-      >
-        <CreditCardIcon />
-        <span> Transactions </span>
-      </ui-button>
-    </router-link>
+  <router-link v-slot="{ isActive }" :to="{ name: ROUTES_NAMES.transactions }">
+    <ui-button
+      :variant="isActive ? 'default' : 'ghost'"
+      as="span"
+      :class="['w-full gap-2 px-3', { 'justify-start': !bottomNav }]"
+      size="default"
+    >
+      <CreditCardIcon />
+      <span> Transactions </span>
+    </ui-button>
+  </router-link>
 
-    <router-link v-slot="{ isActive }" :to="{ name: ROUTES_NAMES.budgets }">
-      <ui-button
-        :variant="isActive ? 'default' : 'ghost'"
-        as="span"
-        :class="['w-full gap-2 px-3', { 'justify-start': !bottomNav }]"
-        size="default"
-      >
-        <Wallet />
-        <span> Budgets </span>
-      </ui-button>
-    </router-link>
-  </nav>
+  <router-link v-slot="{ isActive }" :to="{ name: ROUTES_NAMES.budgets }">
+    <ui-button
+      :variant="isActive ? 'default' : 'ghost'"
+      as="span"
+      :class="['w-full gap-2 px-3', { 'justify-start': !bottomNav }]"
+      size="default"
+    >
+      <Wallet />
+      <span> Budgets </span>
+    </ui-button>
+  </router-link>
 </template>
