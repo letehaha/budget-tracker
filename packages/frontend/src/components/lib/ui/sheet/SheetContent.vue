156486f794ef0eb93d1b51ac5a143d8a0f24ad88
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2025-09-20 22:54:57 +0200

    fix: z-index issues and dialog animation

--- packages/frontend/src/components/lib/ui/sheet/SheetContent.vue
+++ packages/frontend/src/components/lib/ui/sheet/SheetContent.vue
@@ -42,7 +42,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
   <DialogPortal>
     <DialogOverlay
       :class="[
-        'fixed inset-0 z-50 bg-black/80',
+        'fixed inset-0 z-(--z-dialog) bg-black/80',
         'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
       ]"
     />
