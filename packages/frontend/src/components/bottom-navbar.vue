49004a7ccf43c79e5c5da808b609f6a2bedcad12
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2025-12-02 01:02:09 +0100

    fix: Bottom padding on mobile iOS

--- packages/frontend/src/components/bottom-navbar.vue
+++ packages/frontend/src/components/bottom-navbar.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="bg-background sticky right-0 bottom-0 left-0 py-2">
+  <div
+    class="bg-background sticky right-0 bottom-0 left-0 pt-2 pb-[max(0.5rem,env(safe-area-inset-bottom))] max-sm:px-4"
+  >
     <nav class="grid grid-cols-5 gap-1">
       <NavigationLinks bottom-nav />
     </nav>
