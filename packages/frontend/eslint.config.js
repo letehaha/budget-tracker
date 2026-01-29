a06aaa084787568fdf699c9885726ffe041252ab
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2026-01-29 14:04:41 +0100

    fix: flickering upon rendering non-landing pages

--- packages/frontend/eslint.config.js
+++ packages/frontend/eslint.config.js
@@ -6,7 +6,7 @@ import globals from 'globals';
 import typescriptEslint from 'typescript-eslint';
 
 export default typescriptEslint.config(
-  { ignores: ['*.d.ts', '**/coverage', '**/dist', '**/landing/**', '**/legal/**', 'vite.config.js'] },
+  { ignores: ['*.d.ts', '**/coverage', '**/dist', '**/landing/**', '**/legal/**', 'vite.config.js', 'scripts/**'] },
   {
     extends: [
       eslint.configs.recommended,
