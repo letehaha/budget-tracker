0193a9d8d40a8983b518951c5c486cb1cebb579a
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2026-03-08 23:52:36 +0100

    feat: improve accounts combobox

--- packages/frontend/src/components/lib/ui/checkbox/Checkbox.vue
+++ packages/frontend/src/components/lib/ui/checkbox/Checkbox.vue
@@ -1,29 +1,30 @@
 <script setup lang="ts">
 import { cn } from '@/lib/utils';
 import { CheckIcon } from 'lucide-vue-next';
-import type { CheckboxRootEmits, CheckboxRootProps } from 'reka-ui';
-import { CheckboxIndicator, CheckboxRoot, useForwardPropsEmits } from 'reka-ui';
+import type { CheckboxRootProps } from 'reka-ui';
+import { CheckboxIndicator, CheckboxRoot } from 'reka-ui';
 import { type HTMLAttributes, computed } from 'vue';
+import type { CheckedState } from './types';
 
 const props = defineProps<CheckboxRootProps & { class?: HTMLAttributes['class'] }>();
-const emits = defineEmits<CheckboxRootEmits>();
+const emits = defineEmits<{ 'update:modelValue': [value: CheckedState] }>();
 
-const delegatedProps = computed(() => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { class: _, ...delegated } = props;
-
-  return delegated;
+const model = computed({
+  get: () => props.modelValue,
+  set: (val: CheckedState) => emits('update:modelValue', val),
 });
-
-const forwarded = useForwardPropsEmits(delegatedProps, emits);
 </script>
 
 <template>
   <CheckboxRoot
-    v-bind="forwarded"
+    v-model="model"
+    :disabled="props.disabled"
+    :name="props.name"
+    :value="props.value"
+    :id="props.id"
     :class="
       cn(
-        'border-primary ring-offset-background focus-visible:ring-ring data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground peer size-4 shrink-0 rounded-[3px] border focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50',
+        'border-primary ring-offset-background focus-visible:ring-ring data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground data-[state=indeterminate]:bg-primary data-[state=indeterminate]:text-primary-foreground peer size-4 shrink-0 rounded-[3px] border focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50',
         props.class,
       )
     "
